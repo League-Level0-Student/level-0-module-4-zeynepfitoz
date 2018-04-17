6d925bfa1fdebdaa31db8f9705a22414822c0f6c
league <league@wts20.attlocal.net>
2018-04-16 19:55:37 -0700

    change quiz

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -14,15 +14,21 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String nickels= JOptionPane.showInputDialog("How many nickels do you have");
 		// Convert their answer to an int using Integer.parseInt()
-
+int Nickels = Integer.parseInt(nickels);
 		// Ask the user how many dimes they have, and convert their answer
-
+String dimes= JOptionPane.showInputDialog("How many dimes do you have");
 		// Ask the user how many quarters they have, and convert their answer
-
+int Dimes = Integer.parseInt(dimes);
+String Quarters= JOptionPane.showInputDialog("How many quaters do you have");
+int quarters = Integer.parseInt(Quarters);
 		// Calculate how much money the user has and save it in a double variable 
-
+double money = 0.0;
+money+= Nickels*.05;
+money+= Dimes*.10;
+money+= quarters*.25;
+System.out.println("You have $"+money);
 		// Tell the user how much money they have
 
 	}
--- src/FruitQuiz.java
+++ src/FruitQuiz.java
@@ -21,7 +21,7 @@ public class FruitQuiz extends KeyAdapter {
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
-
+System.out.println(keyCode);
 		// 2. Make 3 int variables that hold the key codes for A, b, and C
 		
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
