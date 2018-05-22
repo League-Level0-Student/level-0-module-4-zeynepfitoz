ea8239ff4aaea3eb67bf7cff82359cccc5470e96
AdityaHegde1 <league@wts20.attlocal.net>
2018-05-21 20:00:45 -0700

    # WARNING: head commit changed in the meantime
    methods

---
+++ src/Potato.java
@@ -0,0 +1,21 @@
+
+public class Potato {
+public static void main(String[] args) {
+	for (int i = 1; i < 8; i++) {
+		if(i==1) {
+			
+		
+		System.out.println(i+" "+"potato");
+		}
+		else if() {
+			System.out.println("4");
+
+		}
+		else {
+		
+	
+	System.out.println("more");
+		}
+}
+}
+}
--- src/StringMethods2.java
+++ src/StringMethods2.java
@@ -6,10 +6,12 @@ public class StringMethods2 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it with
-		//    random upper and lower case characters.
-
+		//    random upper and lower case characters
+		String s ="LeMonADe";
+		System.out.println(s.toUpperCase());
 		// 2. Print your String to the console in upper case.
-		
+		System.out.println(s.toLowerCase());
+		System.out.println(s.substring(0,3));
 		// 3. Print your String to the console in lower case.
 
 		// 4. Print the first 3 char's of your String
@@ -17,7 +19,7 @@ public class StringMethods2 {
 		
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+		System.out.println(s.charAt(4));
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
 	}
